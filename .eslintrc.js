1955ec10af69e28ddb76dbac062f5636c15702a8
Jason <jasonintju@gmail.com>
2023-05-21 19:20:29 +0800

    add eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,49 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:import/recommended'
+  ],
+  globals: {
+    process: true,
+    require: true
+  },
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react', '@typescript-eslint'],
+  root: true,
+  rules: {
+    indent: ['error', 2],
+    'no-useless-escape': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'linebreak-style': ['error', 'unix'],
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+          'object',
+          'type'
+        ]
+      }
+    ]
+  }
+};
