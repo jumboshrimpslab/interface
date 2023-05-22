fdf9407006a34e2fe85f78e170c035a2a4e4312b
Jason <jasonintju@gmail.com>
2023-05-22 14:37:20 +0800

    add react-router-dom tainwindcss

---
+++ src/AppRouter.tsx
@@ -0,0 +1,20 @@
+import {
+  Navigate,
+  Route,
+  BrowserRouter as Router,
+  Routes
+} from 'react-router-dom';
+import Home from './pages/Home';
+
+function AppRouter() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default AppRouter;
