5eb9bb602cd5d47e5bceb3e489f665dcc60fb0c8
Jason <jasonintju@gmail.com>
2023-06-16 14:14:18 +0800

    Feat/prize (#7)
    * claim prize api
    
    * check prize
    
    * fix rerender
    
    * remove claim modal

--- src/pages/Home/Banner.tsx
+++ src/pages/Home/Banner.tsx
@@ -6,37 +6,37 @@ import { useModal } from 'hooks';
 import DepositModal from 'components/Modal/DepositModal';
 import { useGlobalLotteryData } from 'contexts/GlobalLotteryDataContext';
 
-const Banner = () => {
+const ButtonBlock = () => {
   const { selectedAccount } = useAccount();
   const { lotteryNotInDrawingFreezeout } = useGlobalLotteryData();
   const { ModalWrapper, showModal, hideModal } = useModal();
-  const ButtonBlock = () => {
-    if (!lotteryNotInDrawingFreezeout) {
-      return (
-        <button className="bg-button-primary/20 border border-primary/50 text-secondary/80 px-[48px] h-[66px] font-title text-xl rounded-xl cursor-default">
-          Deposit for draw #1 has ended
+  if (!lotteryNotInDrawingFreezeout) {
+    return (
+      <button className="bg-button-primary/20 border border-primary/50 text-secondary/80 px-[48px] h-[66px] font-title text-xl rounded-xl cursor-default">
+        Deposit for draw #1 has ended
+      </button>
+    );
+  }
+  if (selectedAccount) {
+    return (
+      <>
+        <button
+          onClick={showModal}
+          className="bg-button-primary w-[280px] h-[66px] font-title text-xl rounded-xl"
+        >
+          Deposit Now
         </button>
-      );
-    }
-    if (selectedAccount) {
-      return (
-        <>
-          <button
-            onClick={showModal}
-            className="bg-button-primary w-[280px] h-[66px] font-title text-xl rounded-xl"
-          >
-            Deposit Now
-          </button>
-          <ModalWrapper>
-            <DepositModal hideModal={hideModal} />
-          </ModalWrapper>
-        </>
-      );
-    }
+        <ModalWrapper>
+          <DepositModal hideModal={hideModal} />
+        </ModalWrapper>
+      </>
+    );
+  }
 
-    return <ConnectWallet btnText="Deposit Now" />;
-  };
+  return <ConnectWallet btnText="Deposit Now" />;
+};
 
+const Banner = () => {
   return (
     <div className="h-[522px] bg-[url(resources/images/banner-background.png)] bg-[length:100%] bg-no-repeat	">
       <img src={BannerImage} width="1440" height="522" className="mx-auto" />
--- src/pages/Home/Prize.tsx
+++ src/pages/Home/Prize.tsx
@@ -1,8 +1,165 @@
-import { useModal } from 'hooks';
-import ClaimPrizesModal from 'components/Modal/ClaimPrizesModal';
+import { useCallback, useEffect, useState } from 'react';
+import BN from 'bn.js';
+import { ProgressSpinner } from 'primereact/progressspinner';
+import classNames from 'classnames';
+import store from 'store';
+import { useUserLotteryData } from 'contexts/UserLotteryDataContext';
+import Balance from 'classes/Balance';
+import PrizeBG from 'resources/images/prize-bg.png';
+import { useSubstrate } from 'contexts/SubstrateContext';
+import { useAccount } from 'contexts/AccountContext';
+import { useGlobalLotteryData } from 'contexts/GlobalLotteryDataContext';
+import type { Signer } from '@polkadot/api/types';
 
 const Prize = () => {
-  const { ModalWrapper, showModal, hideModal } = useModal();
+  const { userUnclaimedWinnings } = useUserLotteryData();
+  const { currentBlockNumber, nextDrawingBlockNumber } = useGlobalLotteryData();
+  const [hasWinning, setHasWinning] = useState(false);
+  const [submitting, setSubmitting] = useState(false);
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+  const { api, apiState } = useSubstrate();
+  const { selectedAccount } = useAccount();
+  const [showCheckPrizeButton, setShowCheckPrizeButton] = useState(false);
+
+  const handleTxRes = (tx: any) => {
+    const status = tx.status;
+    const events = tx.events;
+    console.log('Transaction status:', status.type);
+    if (status.isFinalized) {
+      console.log('Finalized block hash', status.asFinalized.toHex());
+      events.forEach(({ event }: any) => {
+        const { data } = event;
+        if (api?.events.system.ExtrinsicFailed.is(event)) {
+          const error = data[0];
+          if (error.isModule) {
+            const decoded = api.registry.findMetaError(
+              error.asModule.toU8a() as any
+            );
+            const { docs, method, section } = decoded;
+            const errorMsg = `${section}.${method}: ${docs.join(' ')}`;
+            console.error(errorMsg);
+          } else {
+            console.error(error.toString());
+          }
+        }
+      });
+      setIsButtonDisabled(false);
+      setSubmitting(false);
+    }
+  };
+
+  const handleClaim = async () => {
+    setIsButtonDisabled(true);
+    setSubmitting(true);
+    if (!api || apiState !== 'READY' || !selectedAccount) {
+      return;
+    }
+    try {
+      await api.tx.lottery
+        .claimMyWinnings()
+        .signAndSend(
+          selectedAccount?.address,
+          { signer: selectedAccount?.meta.signer as Signer },
+          handleTxRes
+        );
+    } catch (e: any) {
+      console.error(e.message);
+      setIsButtonDisabled(false);
+      setSubmitting(false);
+    }
+  };
+
+  const handleCheck = () => {
+    setShowCheckPrizeButton(false);
+    store.set('nextDrawingBlockNumberAfterChecked', nextDrawingBlockNumber);
+  };
+
+  useEffect(() => {
+    if (userUnclaimedWinnings === null) {
+      setHasWinning(false);
+      return;
+    }
+    if (userUnclaimedWinnings.gt(Balance.Native(new BN(0)))) {
+      setHasWinning(true);
+    } else {
+      setHasWinning(false);
+    }
+  }, [userUnclaimedWinnings]);
+
+  useEffect(() => {
+    if (!currentBlockNumber) {
+      return;
+    }
+    const nextDrawingBlockNumberAfterChecked = store.get(
+      'nextDrawingBlockNumberAfterChecked'
+    );
+    if (
+      !nextDrawingBlockNumberAfterChecked ||
+      currentBlockNumber >= nextDrawingBlockNumberAfterChecked
+    ) {
+      setShowCheckPrizeButton(true);
+    } else {
+      setShowCheckPrizeButton(false);
+    }
+  }, [currentBlockNumber, userUnclaimedWinnings]);
+
+  const ButtonBlock = useCallback(() => {
+    if (!selectedAccount) {
+      return (
+        <button className="w-[280px] h-[66px] rounded-xl text-xl font-title border border-white/50 bg-white/20 text-white/80 cursor-not-allowed">
+          No Draws to Check
+        </button>
+      );
+    } else {
+      // 3 button types: 'Check Prize' or 'Claim Prize' or 'No Draws to Check'
+      if (showCheckPrizeButton) {
+        return (
+          <button
+            onClick={handleCheck}
+            className="bg-button-primary font-title w-[280px] rounded-xl h-[66px] text-xl text-white flex items-center justify-center gap-4"
+          >
+            Check Prize
+          </button>
+        );
+      } else if (hasWinning) {
+        return (
+          <button
+            onClick={handleClaim}
+            disabled={isButtonDisabled}
+            className={classNames(
+              'font-title w-[280px] rounded-xl h-[66px] text-xl text-white flex items-center justify-center gap-4',
+              {
+                'bg-button-primary': !isButtonDisabled,
+                'border border-primary/50 bg-button-primary/70 cursor-not-allowed':
+                  isButtonDisabled
+              }
+            )}
+          >
+            {submitting ? 'Claiming' : 'Claim Prize'}
+            {submitting && (
+              <ProgressSpinner
+                className="w-[32px] h-[32px] m-0"
+                strokeWidth="4"
+              />
+            )}
+          </button>
+        );
+      } else {
+        return (
+          <button className="w-[280px] h-[66px] rounded-xl text-xl font-title border border-white/50 bg-white/20 text-white/80 cursor-not-allowed">
+            No Draws to Check
+          </button>
+        );
+      }
+    }
+  }, [
+    selectedAccount,
+    showCheckPrizeButton,
+    hasWinning,
+    isButtonDisabled,
+    submitting
+  ]);
+
   return (
     <div>
       <div className="h-[202px] bg-gradient-to-r from-[#FF6B00] to-[#FFCE51] rounded-3xl flex items-center justify-between px-12 text-tertiary">
@@ -10,22 +167,23 @@ const Prize = () => {
           <div className="font-content text-2xl leading-[30px] font-extrabold mb-2">
             Coming Soon
           </div>
-          <div className="font-title text-[40px] leading-[68px]">Draw #4</div>
+          <div className="font-title text-[40px] leading-[68px]">Draw #1</div>
         </div>
-        <div>
-          {/* <button className="w-[280px] h-[66px] rounded-xl text-xl font-title border border-white/50 bg-white/20 text-white/80 cursor-not-allowed">
-            No Draws to Check
-          </button> */}
-          <button
-            onClick={showModal}
-            className="w-[280px] h-[66px] rounded-xl text-xl font-title bg-button-primary text-white"
-          >
-            Claim Prize
-          </button>
-          <ModalWrapper>
-            <ClaimPrizesModal hideModal={hideModal} />
-          </ModalWrapper>
-        </div>
+
+        {!showCheckPrizeButton && hasWinning && (
+          <div className="relative -rotate-[16deg]">
+            <span className="absolute font-title text-[32px] text-white left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+              {userUnclaimedWinnings?.toString(2)}
+            </span>
+            <img
+              src={PrizeBG}
+              alt="prize background"
+              width="226"
+              height="226"
+            />
+          </div>
+        )}
+        <ButtonBlock />
       </div>
       <div>
         <div className="font-title text-xl leading-[34px] mt-6 mb-4">
@@ -45,7 +203,10 @@ const Prize = () => {
             Prize
           </span>
         </div>
-        <div className="font-content text-base flex gap-5 mt-2">
+        <div className="bg-primary h-[30px] leading-[30px] font-content mt-2 text-base">
+          No prize history yet
+        </div>
+        {/* <div className="font-content text-base flex gap-5 mt-2">
           <span className="bg-primary h-[30px] flex-1 rounded-[6px] flex items-center justify-center gap-4">
             Draw #3
           </span>
@@ -58,35 +219,7 @@ const Prize = () => {
           <span className="bg-primary h-[30px] leading-[30px] flex-1 rounded-[6px]">
             10000 MANTA
           </span>
-        </div>
-        <div className="font-content text-base flex gap-5 mt-2">
-          <span className="bg-primary h-[30px] flex-1 rounded-[6px] flex items-center justify-center gap-4">
-            Draw #2
-          </span>
-          <span className="bg-primary h-[30px] leading-[30px] flex-1 rounded-[6px]">
-            Dec 12, 2022 at 12:00 AM
-          </span>
-          <span className="bg-primary h-[30px] leading-[30px] flex-1 rounded-[6px]">
-            Vdu4a...3dMV
-          </span>
-          <span className="bg-primary h-[30px] leading-[30px] flex-1 rounded-[6px]">
-            10000 MANTA
-          </span>
-        </div>
-        <div className="font-content text-base flex gap-5 mt-2">
-          <span className="bg-primary h-[30px] flex-1 rounded-[6px] flex items-center justify-center gap-4">
-            Draw #1
-          </span>
-          <span className="bg-primary h-[30px] leading-[30px] flex-1 rounded-[6px]">
-            Dec 12, 2022 at 12:00 AM
-          </span>
-          <span className="bg-primary h-[30px] leading-[30px] flex-1 rounded-[6px]">
-            Vdu4a...3dMV
-          </span>
-          <span className="bg-primary h-[30px] leading-[30px] flex-1 rounded-[6px]">
-            10000 MANTA
-          </span>
-        </div>
+        </div> */}
       </div>
     </div>
   );
